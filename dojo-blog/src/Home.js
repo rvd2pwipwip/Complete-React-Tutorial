3c945daa8481b25932f860e49949b51950582811
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-30 06:53:39 -0500

    07-click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+  const handleClick = (e, name) => {
+    console.log(e.target, `hello, ${name}`);
+  };
+  const name = "dude"
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={(e) => handleClick(e, name)}>Click me</button>
     </div>
   );
 };
