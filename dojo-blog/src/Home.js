7179d8b81eb42ae5503489b49d1263a77d5e7cf0
Herve Desrosiers <hdesrosiers@stingray.com>
2021-12-02 07:14:15 -0500

    19-handling fetch errors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,23 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setBlogs(data)
-        setIsPending(false)
-      });
-    }, 1000
-    );
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null)
+        })
+        .catch((e) => {
+          setError(e.message)
+          setIsPending(false)
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
