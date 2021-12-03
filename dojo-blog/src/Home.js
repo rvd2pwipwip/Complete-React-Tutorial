2bbcf7d74005309433e9016d173edb0587bf4ac1
Herve Desrosiers <hdesrosiers@stingray.com>
2021-12-03 06:32:46 -0500

    20-making a custom hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,31 +1,13 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((response) => {
-          if (!response.ok) {
-            throw Error("Could not fetch the data for that resource");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null)
-        })
-        .catch((e) => {
-          setError(e.message)
-          setIsPending(false)
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((e) => {
+          setError(e.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
